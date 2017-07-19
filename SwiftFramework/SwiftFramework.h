2c3cc1180d3209dafcc2e0cea2f008a21423d2c0
zl00 <zl00_00@yeah.net>
2017-07-19 15:20:30 +0800

    Initial Commit

---
+++ SwiftFramework/SwiftFramework.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFramework.h
+//  SwiftFramework
+//
+//  Created by Linda Zhong on 19/07/2017.
+//  Copyright © 2017 Null. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftFramework.
+FOUNDATION_EXPORT double SwiftFrameworkVersionNumber;
+
+//! Project version string for SwiftFramework.
+FOUNDATION_EXPORT const unsigned char SwiftFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFramework/PublicHeader.h>
+
+
